1a186d7f867cfc7ebc2e5387283ae48e3a0493cc
bujosa <davidbujosa@gmail.com>
2021-02-03 02:34:38 -0400

    Eliminated bad files

--- chat-server/models/sockets.js
+++ chat-server/models/sockets.js
@@ -1,11 +1,6 @@
-const TicketList = require("./ticket-list");
-
 class Sockets {
   constructor(io) {
     this.io = io;
-
-    this.ticketList = new TicketList();
-
     this.socketEvents();
   }
 
@@ -13,21 +8,6 @@ class Sockets {
     // On connection
     this.io.on("connection", (socket) => {
       console.log("Cliente conectado");
-
-      socket.on("new-ticket", (data, callBack) => {
-        const newTicket = this.ticketList.createTicket();
-        callBack(newTicket);
-      });
-
-      socket.on("next-ticket", (user, callBack) => {
-        const yourTicket = this.ticketList.toAssignTicket(
-          user.agent,
-          user.desktop
-        );
-        callBack(yourTicket);
-
-        this.io.emit("ticket-assigned", this.ticketList.lastThirteen);
-      });
     });
   }
 }
