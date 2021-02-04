09c7581fd0b59e522f64c29b40dafeac820c787e
bujosa <davidbujosa@gmail.com>
2021-02-04 03:22:39 -0400

    Added incoming message component

--- chat-app/src/components/chat/Messages.js
+++ chat-app/src/components/chat/Messages.js
@@ -1,26 +1,13 @@
 import React from "react";
+import { IncomingMessage } from "./IncomingMessage";
+import { SendMessage } from "./SendMessage";
 
 export const Messages = () => {
   return (
     <div className="mesgs">
       {/* <!-- Historia inicio --> */}
       <div className="msg_history">
-        {/* <!-- Mensaje recibido Inicio --> */}
-        <div className="incoming_msg">
-          <div className="incoming_msg_img">
-            <img
-              src="https://ptetutorials.com/images/user-profile.png"
-              alt="sunil"
-            />
-          </div>
-          <div className="received_msg">
-            <div className="received_withd_msg">
-              <p>Test which is a new approach to have all solutions</p>
-              <span className="time_date"> 11:01 AM | June 9</span>
-            </div>
-          </div>
-        </div>
-        {/* <!-- Mensaje recibido Fin --> */}
+        <IncomingMessage />
 
         {/* <!-- Mensaje enviado inicio --> */}
         <div className="outgoing_msg">
@@ -32,6 +19,7 @@ export const Messages = () => {
         {/* <!-- Mensaje enviado inicio --> */}
       </div>
       {/* <!-- Historia Fin --> */}
+      <SendMessage />
     </div>
   );
 };
