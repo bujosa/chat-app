ddd44e4217da63bc8b18520cf296e6628ab8367c
bujosa <davidbujosa@gmail.com>
2021-02-04 03:27:01 -0400

    Added chat select component

---
+++ chat-app/src/components/chat/ChatSelect.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const ChatSelect = () => {
+  return (
+    <div className="middle-screen">
+      <div className="alert-info">
+        <hr />
+        <h3>Select a chat</h3>
+        <span>To start</span>
+      </div>
+    </div>
+  );
+};
