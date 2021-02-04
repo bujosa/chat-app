bbad0d44068161a1f68b777a3448e44663f25c8e
bujosa <davidbujosa@gmail.com>
2021-02-04 03:24:05 -0400

    Added outgoing message component

--- chat-app/src/components/chat/Messages.js
+++ chat-app/src/components/chat/Messages.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { IncomingMessage } from "./IncomingMessage";
+import { OutgoingMessage } from "./OutgoingMessage";
 import { SendMessage } from "./SendMessage";
 
 export const Messages = () => {
@@ -8,15 +9,7 @@ export const Messages = () => {
       {/* <!-- Historia inicio --> */}
       <div className="msg_history">
         <IncomingMessage />
-
-        {/* <!-- Mensaje enviado inicio --> */}
-        <div className="outgoing_msg">
-          <div className="sent_msg">
-            <p>Test which is a new approach to have all solutions</p>
-            <span className="time_date"> 11:01 AM | June 9</span>
-          </div>
-        </div>
-        {/* <!-- Mensaje enviado inicio --> */}
+        <OutgoingMessage />
       </div>
       {/* <!-- Historia Fin --> */}
       <SendMessage />
