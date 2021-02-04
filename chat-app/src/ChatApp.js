6f14acb8fdf6062ae90fea2022e947ede14990e1
bujosa <davidbujosa@gmail.com>
2021-02-04 02:26:32 -0400

    Added chat app

---
+++ chat-app/src/ChatApp.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ChatApp = () => {
+  return <div>Chat App</div>;
+};
---
+++ chat-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChatApp } from "./ChatApp";
+import "./index.css";
+
+ReactDOM.render(<ChatApp />, document.getElementById("root"));
