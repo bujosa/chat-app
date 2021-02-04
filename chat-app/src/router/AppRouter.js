60a457bd58d4fecad300fbd3e6d94e4e5f31d7b6
bujosa <davidbujosa@gmail.com>
2021-02-04 02:36:42 -0400

    Added routes on app router

--- chat-app/src/router/AppRouter.js
+++ chat-app/src/router/AppRouter.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { ChatPage } from "../pages/ChatPage";
+import { LoginPage } from "../pages/LoginPage";
+import { RegisterPage } from "../pages/RegisterPage";
 
 export const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
+          <Route path="/register" component={ChatPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
 };
