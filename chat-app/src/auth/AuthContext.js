1b5ba3b0460da5675e7a39144529a2eca63ce34c
bujosa <davidbujosa@gmail.com>
2021-02-05 02:33:13 -0400

    Added function on auth context

--- chat-app/src/auth/AuthContext.js
+++ chat-app/src/auth/AuthContext.js
@@ -1,3 +1,29 @@
-import { createContext } from "react";
+import React, { useState, createContext, useCallback } from "react";
 
 export const AuthContext = createContext();
+
+const initialState = {
+  uid: null,
+  checking: true,
+  logged: false,
+  name: null,
+  email: null,
+};
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(initialState);
+
+  const login = (email, password) => {};
+
+  const register = (email, name, password) => {};
+
+  const tokenVerify = useCallback(() => {}, []);
+
+  const logout = () => {};
+
+  return (
+    <AuthContext.Provider value={{ logout, login, tokenVerify, register }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
