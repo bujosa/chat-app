d689c9cfe99d5bde1866c4109ac7d4e20c900e54
bujosa <davidbujosa@gmail.com>
2021-02-05 05:01:42 -0400

    Added logout function

--- chat-app/src/components/chat/Searchbox.js
+++ chat-app/src/components/chat/Searchbox.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../auth/AuthContext";
 
 export const SearchBox = () => {
+  const { auth, logout } = useContext(AuthContext);
   return (
     <div className="headind_srch">
       <div className="recent_heading mt-2">
-        <h4>Recientes</h4>
+        <h4>{auth.name}</h4>
       </div>
       <div className="srch_bar">
         <div className="stylish-input-group">
-          <button className="btn text-danger">Salir</button>
+          <button className="btn text-danger" onClick={logout}>
+            Salir
+          </button>
         </div>
       </div>
     </div>
