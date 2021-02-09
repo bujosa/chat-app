75a2ad1a52c0d782a4dfc270e12f2e29709ad6b7
bujosa <davidbujosa@gmail.com>
2021-02-09 02:16:36 -0400

    Added chat reducer function

--- chat-app/src/ChatApp.js
+++ chat-app/src/ChatApp.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { AuthProvider } from "./auth/AuthContext";
+import { ChatProvider } from "./context/chat/ChatContext";
 import { SocketProvider } from "./context/SocketContext";
 import { AppRouter } from "./router/AppRouter";
 
 export const ChatApp = () => {
   return (
     <div>
-      <AuthProvider>
-        <SocketProvider>
-          <AppRouter />
-        </SocketProvider>
-      </AuthProvider>
+      <ChatProvider>
+        <AuthProvider>
+          <SocketProvider>
+            <AppRouter />
+          </SocketProvider>
+        </AuthProvider>
+      </ChatProvider>
     </div>
   );
 };
--- chat-app/src/context/chat/ChatContext.js
+++ chat-app/src/context/chat/ChatContext.js
@@ -1,6 +1,13 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const ChatContext = createContext();
+
+const initialState = {
+  uid: "",
+  activeChat: null,
+  users: [],
+  messages: [],
+};
 
 export const ChatProvider = ({ children }) => {
   return <ChatContext.Provider>{children}</ChatContext.Provider>;
