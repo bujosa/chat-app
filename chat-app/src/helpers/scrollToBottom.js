961ad4b831bbe32deefd8e05f7234d09046b883f
bujosa <davidbujosa@gmail.com>
2021-02-09 04:25:54 -0400

    Added scroll to bottom

---
+++ chat-app/src/helpers/scrollToBottom.js
@@ -0,0 +1,14 @@
+import { animateScroll } from "react-scroll";
+export const scrollToBottom = (id) => {
+  animateScroll.scrollToBottom({
+    containerId: id,
+    duration: 0,
+  });
+};
+
+export const scrollToBottomAnimated = (id) => {
+  animateScroll.scrollToBottom({
+    containerId: id,
+    duration: 250,
+  });
+};
